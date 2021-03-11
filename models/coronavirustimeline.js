4f65dc992cdd99f29f6a3e4e57c32207b22cb588
DESKTOP-RKTFKHS\airu8 <airu85@live.jp>
2021-03-11 18:52:17 +0900

    First Commit

--- models/coronavirustimeline.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const CoronavirustimelineSchema = new mongoose.Schema({
-    cornavirusoverall: Object,
-    gotDate: Date,
-});
-
-module.exports = mongoose.model("Coronavirustimeline", CoronavirustimelineSchema);
